3811e0a3d40b77d47e8c7fda145fdbc29030ee87
themdebrahim98 <mdebrahim742148@gmail.com>
2024-04-17 15:12:07 +0530

    add hostname

--- client/src/util.js
+++ client/src/util.js
@@ -1,2 +1,2 @@
-// export const hostname = `${window.location.origin}/api`
-export const hostname = `http://localhost:8800/api`
+export const hostname = `${window.location.origin}/api`;
+// export const hostname = `http://localhost:8800/api`
