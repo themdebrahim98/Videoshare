5fdca0ba276fd61997a41dae67565f7f2eb0da3b
themdebrahim98 <mdebrahim742148@gmail.com>
2024-04-17 16:39:05 +0530

    bild command added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import url from "url";
 
 const PORT = 8800 || process.env.PORT;
 const app = express();
@@ -37,17 +39,27 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/video", videoRoutes);
 app.use("/api/comment", commentRoutes);
-
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "something went wrong!";
   next();
-
   return res.status(status).json({
     success: status,
     status: status,
     message: message,
   });
+});
+
+export function currDir(fileUrl) {
+  const __filename = url.fileURLToPath(fileUrl);
+  return path.dirname(__filename);
+}
+const __dirname = currDir(import.meta.url);
+console.log(__dirname);
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
